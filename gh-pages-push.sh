123e2343c1612f3d399cc122ff3bc62e10de56af
M0ose <m0ose@yahoo.com>
2014-11-04 09:33:34 -0700

    commiting changes

--- gh-pages-push.sh
+++ gh-pages-push.sh
@@ -4,15 +4,15 @@ repo=ecostem-sand
 
 mkdir gh_pages_tmp && \
 pushd gh_pages_tmp && \
-  git clone git@github.com:simtable/$repo.git && \
+  git clone git@github.com:m0ose/$repo.git && \
   pushd $repo && \
     (git push origin --delete gh-pages || true ) && \
     git branch gh-pages && \
     git checkout gh-pages && \
     git pull origin master && \
-    git clone git@github.com:simtable/st-api.git && \
-    rm -rf st-api/.git && \
-    git add st-api && \
+    #git clone git@github.com:m0ose/st-api.git && \
+    #rm -rf st-api/.git && \
+    #git add st-api && \
     (git commit -a -m 'update' || true) && \
     (git push origin gh-pages || true) && \
   popd && \
